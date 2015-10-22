77377e83c4cb6dfd39a906133a017b718c217df4
guo <smile_struggle@outlook.com>
2015-10-22 17:31:30 +0800

    test v2

--- designModel/src/com/cargosmart/csmcs/report/entrance/Entrance.java
+++ designModel/src/com/cargosmart/csmcs/report/entrance/Entrance.java
@@ -1,8 +1,9 @@
 package com.cargosmart.csmcs.report.entrance;
 
 import java.util.Calendar;
+import java.util.HashMap;
 import java.util.List;
-
+import java.util.Map;
 import org.apache.log4j.Logger;
 
 import com.cargosmart.csmcs.report.domain.Clientusagedata;
@@ -16,98 +17,39 @@ public class Entrance {
 
 	private DataLoadTools dataLoadTools = new DataLoadTools();
 
-	private long manual_searchWithoutFurtherAction = 0;
-	private long manual_searchReliability = 0;
-	private long manual_showMap = 0;
-	private long manual_showMap_refineSearch = 0;
-	private long manual_showMap_scheduleReliability = 0;
-	private long manual_showMap_refineSearch_scheduleReliability = 0;
-	private long favorite_searchWithoutFurtherAction = 0;
-	private long favorite_searchReliability = 0;
-	private long favorite_showMap = 0;
-	private long favorite_showMap_refineSearch = 0;
-	private long favorite_showMap_scheduleReliability = 0;
-	private long favorite_showMap_refineSearch_scheduleReliability = 0;
-	private long favorite_others = 0;
-	private long manual_others = 0;
-	private long favorite_refineSearch = 0;
-	private long manual_refineSearch = 0;
-
-	private long manual_searchReliability_mobile;
-
-	private long manual_searchReliability_web;
-
-	private long manual_showMap_mobile;
-
-	private long manual_showMap_web;
-
-	private long manual_showMap_refineSearch_mobile;
-
-	private long manual_showMap_refineSearch_web;
-
-	private long manual_showMap_scheduleReliability_mobile;
-
-	private long manual_showMap_scheduleReliability_web;
-
-	private long manual_showMap_refineSearch_scheduleReliability_mobile;
-
-	private long manual_showMap_refineSearch_scheduleReliability_web;
-
-	private long manual_refineSearch_mobile;
-
-	private long manual_refineSearch_web;
-
-	private long manual_searchWithoutFurtherAction_mobile;
-
-	private long manual_searchWithoutFurtherAction_web;
-
-	private long manual_others_mobile;
-
-	private long manual_others_web;
-
-	private long favorite_searchReliability_mobile;
-
-	private long favorite_searchReliability_web;
-
-	private long favorite_showMap_mobile;
-
-	private long favorite_showMap_web;
-
-	private long favorite_showMap_refineSearch_mobile;
-
-	private long favorite_showMap_refineSearch_web;
-
-	private long favorite_showMap_scheduleReliability_mobile;
-
-	private long favorite_showMap_scheduleReliability_web;
-
-	private long favorite_showMap_refineSearch_scheduleReliability_mobile;
-
-	private long favorite_showMap_refineSearch_scheduleReliability_web;
-
-	private long favorite_refineSearch_mobile;
-
-	private long favorite_refineSearch_web;
-
-	private long favorite_searchWithoutFurtherAction_mobile;
-
-	private long favorite_searchWithoutFurtherAction_web;
-
-	private long favorite_others_mobile;
-
-	private long favorite_others_web;
-
+	int[] refineSearchCounts = {0, 0, 0, 0, 0};
+	int[] scheduleReliabilityCounts = {0, 0, 0, 0, 0};
+	int[] showMapCounts = {0, 0, 0, 0, 0};
+	int[] showMaprefineSearchCounts = {0, 0, 0, 0, 0};
+	int[] showMapscheduleReliabilityCounts = {0, 0, 0, 0, 0};
+	int[] showMap3Counts = {0, 0, 0, 0, 0};
+	int[] othersCounts = {0, 0, 0, 0, 0};
+	
 	public void first() {
 		List<String> registerUserIDs = dataLoadTools.getRegistUserIDs();
+		
+		//querySearchByUserID(registerUserIDs.get(0));
+		
+		for (String uid:  registerUserIDs) {
+			querySearchByUserID(uid);
+		}
+		pintArray(refineSearchCounts);
+		pintArray(scheduleReliabilityCounts);
+		pintArray(showMapCounts);
+		pintArray(showMaprefineSearchCounts);
+		pintArray(showMapscheduleReliabilityCounts);
+		pintArray(showMap3Counts);
+		pintArray(othersCounts);
+		
 		// List<String> publishUsersIDs=dataLoadTools.getPublishUserIDs();
-		byManualSearch(registerUserIDs);
-		byFavoriteSearch(registerUserIDs);
+		//byManualSearch(registerUserIDs);
+//		byFavoriteSearch(registerUserIDs);
 		// dataLoadTools.setSearchByIP(true);
 		// byManualSearch(publishUsersIDs);
 		
 		
 		//by manual search
-		System.out.println("by manual/favorite");
+		/*System.out.println("by manual/favorite");
 		System.out.println("manual_searchWithoutFurtherAction:" + manual_searchWithoutFurtherAction);
 		System.out.println("manual_searchReliability:" + manual_searchReliability);
 		System.out.println("manual_showMap:" + manual_showMap);
@@ -159,7 +101,7 @@ public class Entrance {
 		System.out.println("favorite_searchWithoutFurtherAction_mobile:"+favorite_searchWithoutFurtherAction_mobile);
 		System.out.println("favorite_searchWithoutFurtherAction_web:"+favorite_searchWithoutFurtherAction_web);
 		System.out.println("favorite_others_mobile:"+favorite_others_mobile);
-		System.out.println("favorite_others_web:"+favorite_others_web);
+		System.out.println("favorite_others_web:"+favorite_others_web);*/
 		
 	}
 
@@ -170,6 +112,141 @@ public class Entrance {
 		System.out.println("spent time "+(end-begin));
 	}
 
+	public boolean inArray(String[] arr, String t) {
+		for (String s : arr) {
+			if (s.equals(t)) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public void querySearchByUserID(String userID) {
+		System.out.println(userID);
+		
+		String[] manualSearchCodes = {"trace_routes_search","trace_main_routes_searchv","trace_main_search"};
+		String[] favoriteSearchCodes = {"trace_routes_favorite_item", "trace_main_favorite_item"};
+		String[] withoutFurtherCodes = {"trace_routes_firstThingAfterSearch"};
+		String[] scheduleReliabilityCodes = {"trace_routes_selectSSRRPortPair","trace_routes_clickSSRRImage"};
+		String[] showMapCodes = {"trace_routes_selectDetail"};
+		String[] refineSearchCodes = {"trace_routes_selectCalendar","trace_routes_selectDirect","trace_routes_selectCycutoffCalendar","trace_routes_selectArrivalCalendar","trace_routes_selectDepartureCalendar","trace_routes_changeTransitTime"};
+
+		List<Clientusagedata> searchedRecords = dataLoadTools.allSearch(userID);
+		Map<Clientusagedata, Map<String, Integer>>  userActionTacingMap = new HashMap<Clientusagedata, Map<String, Integer>>();
+//		String currentSearchID = null;
+		Map<String, Integer> currentSearchMap = null;
+		Calendar actionStopCal = null;
+		for (Clientusagedata ud : searchedRecords) {
+			if (inArray(manualSearchCodes, ud.getFunc()) ||  inArray(favoriteSearchCodes, ud.getFunc())){
+//				currentSearchID = ud.getId();
+				currentSearchMap = new HashMap<String, Integer>();
+				userActionTacingMap.put(ud, currentSearchMap);
+				
+				actionStopCal = Calendar.getInstance();
+				actionStopCal.setTime(ud.getCreateTime());
+				actionStopCal.add(Calendar.MINUTE, 30);
+				
+			} else {
+				if (currentSearchMap != null) {
+					
+					Calendar cal2= Calendar.getInstance();
+					cal2.setTime(ud.getCreateTime());
+				    
+					if (cal2.before(actionStopCal)) {
+						String action = ud.getFunc();
+						int c = currentSearchMap.containsKey(action) ? currentSearchMap.get(action) + 1 : 1;
+						currentSearchMap.put(action, c);
+					}
+				}
+			}
+		}
+		
+		
+		
+		for (Map.Entry<Clientusagedata, Map<String, Integer>> entry : userActionTacingMap.entrySet()) {
+			Clientusagedata ud = entry.getKey();
+			Map<String, Integer> followingActionMap = entry.getValue();
+			
+			
+			int idx1 = inArray(manualSearchCodes, ud.getFunc()) ? 2: 3;
+			int idx2 = isByMobile(ud) ? 1 : 0;
+			
+			boolean hasScheduleReliability =false;
+			boolean hasRefineSearch = false;
+			boolean hasShowMap = false;
+			
+			for (String action: followingActionMap.keySet()) {
+				if (inArray(showMapCodes, action)) {
+					hasShowMap = true;
+				} else if (inArray(refineSearchCodes, action)) {
+					hasRefineSearch = true;
+				} else if (inArray(scheduleReliabilityCodes, action)) {
+					hasScheduleReliability = true;
+				}
+			}
+			
+			if (hasRefineSearch){
+				refineSearchCounts[idx1] += 1;
+				refineSearchCounts[idx2] += 1;
+				refineSearchCounts[4] += 1;
+			}
+			if (hasScheduleReliability) {
+				scheduleReliabilityCounts[idx1] += 1;
+				scheduleReliabilityCounts[idx2] += 1;
+				scheduleReliabilityCounts[4] += 1;
+			}
+			if (hasShowMap) {
+				showMapCounts[idx1] += 1;
+				showMapCounts[idx2] += 1;
+				showMapCounts[4] += 1;
+			}
+			if (hasShowMap && hasRefineSearch) {
+				showMaprefineSearchCounts[idx1] += 1;
+				showMaprefineSearchCounts[idx2] += 1;
+				showMaprefineSearchCounts[4] += 1;
+			}
+			if (hasShowMap && hasScheduleReliability) {
+				showMapscheduleReliabilityCounts[idx1] += 1;
+				showMapscheduleReliabilityCounts[idx2] += 1;
+				showMapscheduleReliabilityCounts[4] += 1;
+			}
+			if (hasShowMap && hasRefineSearch && hasScheduleReliability) {
+				showMap3Counts[idx1] += 1;
+				showMap3Counts[idx2] += 1;
+				showMap3Counts[4] += 1;
+			}
+			if (!(hasShowMap || hasRefineSearch || hasScheduleReliability)) {
+				othersCounts[idx1] += 1;
+				othersCounts[idx2] += 1;
+				othersCounts[4] += 1;
+			}
+					
+		}
+		
+		/*pintArray(refineSearchCounts);
+		pintArray(scheduleReliabilityCounts);
+		pintArray(showMapCounts);
+		pintArray(showMaprefineSearchCounts);
+		pintArray(showMapscheduleReliabilityCounts);
+		pintArray(showMap3Counts);
+		pintArray(othersCounts);*/
+		
+	}
+	
+	public void pintArray(int[] arr) {
+		StringBuffer sb = new StringBuffer();
+		for (int i = 0; i < arr.length; i++) {
+			if (i > 0) {
+				sb.append(",");
+			}
+			sb.append(arr[i]);
+		}
+		System.out.println(sb.toString());
+	}
+	public boolean isByMobile(Clientusagedata ud) {
+		return ud.getRequestInformation_requestHeaders_user_agent().matches("^Big Schedules.*iPhone' and '^Apache-HttpClient");
+	}
+	
 	public void byManualSearch(List<String> registerUserIDs) {
 		
 		for (String id : registerUserIDs) {
@@ -215,69 +292,45 @@ public class Entrance {
 						calendar);
 
 				if (isSearchReliability == true && isShowmap == false && isRefineSearch == false) {
-					manual_searchReliability++;
 					// channel web or mobile
 					if(firstSearchRecord.getRequestInformation_requestHeaders_user_agent().matches("^Big Schedules.*iPhone' and '^Apache-HttpClient")){
-						manual_searchReliability_mobile ++;
 					}else{
-						manual_searchReliability_web++;
 					}
 				} else if (isShowmap == true && isRefineSearch == false && isSearchReliability == false) {
-					manual_showMap++;
 					// channel web or mobile
 					if(firstSearchRecord.getRequestInformation_requestHeaders_user_agent().matches("^Big Schedules.*iPhone' and '^Apache-HttpClient")){
-						manual_showMap_mobile ++;
 					}else{
-						manual_showMap_web++;
 					}
 				} else if (isShowmap == true && isRefineSearch == true && isSearchReliability == false) {
-					manual_showMap_refineSearch++;
 					// channel web or mobile
 					if(firstSearchRecord.getRequestInformation_requestHeaders_user_agent().matches("^Big Schedules.*iPhone' and '^Apache-HttpClient")){
-						manual_showMap_refineSearch_mobile ++;
 					}else{
-						manual_showMap_refineSearch_web++;
 					}
 				} else if (isShowmap == true && isSearchReliability == true && isRefineSearch == false) {
-					manual_showMap_scheduleReliability++;
 					// channel web or mobile
 					if(firstSearchRecord.getRequestInformation_requestHeaders_user_agent().matches("^Big Schedules.*iPhone' and '^Apache-HttpClient")){
-						manual_showMap_scheduleReliability_mobile ++;
 					}else{
-						manual_showMap_scheduleReliability_web++;
 					}
 				} else if (isShowmap == true && isRefineSearch == true && isSearchReliability == true) {
-					manual_showMap_refineSearch_scheduleReliability++;
 					// channel web or mobile
 					if(firstSearchRecord.getRequestInformation_requestHeaders_user_agent().matches("^Big Schedules.*iPhone' and '^Apache-HttpClient")){
-						manual_showMap_refineSearch_scheduleReliability_mobile ++;
 					}else{
-						manual_showMap_refineSearch_scheduleReliability_web++;
 					}
 				} else if (isRefineSearch == true && isShowmap == false && isSearchReliability == false) {
-					manual_refineSearch++;
 					// channel web or mobile
 					if(firstSearchRecord.getRequestInformation_requestHeaders_user_agent().matches("^Big Schedules.*iPhone' and '^Apache-HttpClient")){
-						manual_refineSearch_mobile ++;
 					}else{
-						manual_refineSearch_web++;
 					}
 				} else if (isWithoutFurtherAction == true && isRefineSearch == false && isSearchReliability == false
 						&& isShowmap == false) {
-					manual_searchWithoutFurtherAction++;
 					// channel web or mobile
 					if(firstSearchRecord.getRequestInformation_requestHeaders_user_agent().matches("^Big Schedules.*iPhone' and '^Apache-HttpClient")){
-						manual_searchWithoutFurtherAction_mobile ++;
 					}else{
-						manual_searchWithoutFurtherAction_web++;
 					}
 				} else {
-					manual_others++;
 					// channel web or mobile
 					if(firstSearchRecord.getRequestInformation_requestHeaders_user_agent().matches("^Big Schedules.*iPhone' and '^Apache-HttpClient")){
-						manual_others_mobile ++;
 					}else{
-						manual_others_web++;
 					}
 				}
 
@@ -356,61 +409,37 @@ public class Entrance {
 						calendar);
 
 				if (isSearchReliability == true && isShowmap == false && isRefineSearch == false) {
-					favorite_searchReliability++;
 					if(firstSearchRecord.getRequestInformation_requestHeaders_user_agent().matches("^Big Schedules.*iPhone' and '^Apache-HttpClient")){
-						favorite_searchReliability_mobile ++;
 					}else{
-						favorite_searchReliability_web++;
 					}
 				} else if (isShowmap == true && isRefineSearch == false && isSearchReliability == false) {
-					favorite_showMap++;
 					if(firstSearchRecord.getRequestInformation_requestHeaders_user_agent().matches("^Big Schedules.*iPhone' and '^Apache-HttpClient")){
-						favorite_showMap_mobile ++;
 					}else{
-						favorite_showMap_web++;
 					}
 				} else if (isShowmap == true && isRefineSearch == true && isSearchReliability == false) {
-					favorite_showMap_refineSearch++;
 					if(firstSearchRecord.getRequestInformation_requestHeaders_user_agent().matches("^Big Schedules.*iPhone' and '^Apache-HttpClient")){
-						favorite_showMap_refineSearch_mobile ++;
 					}else{
-						favorite_showMap_refineSearch_web++;
 					}
 				} else if (isShowmap == true && isSearchReliability == true && isRefineSearch == false) {
-					favorite_showMap_scheduleReliability++;
 					if(firstSearchRecord.getRequestInformation_requestHeaders_user_agent().matches("^Big Schedules.*iPhone' and '^Apache-HttpClient")){
-						favorite_showMap_scheduleReliability_mobile ++;
 					}else{
-						favorite_showMap_scheduleReliability_web++;
 					}
 				} else if (isShowmap == true && isRefineSearch == true && isSearchReliability == true) {
-					favorite_showMap_refineSearch_scheduleReliability++;
 					if(firstSearchRecord.getRequestInformation_requestHeaders_user_agent().matches("^Big Schedules.*iPhone' and '^Apache-HttpClient")){
-						favorite_showMap_refineSearch_scheduleReliability_mobile ++;
 					}else{
-						favorite_showMap_refineSearch_scheduleReliability_web++;
 					}
 				} else if (isRefineSearch == true && isShowmap == false && isSearchReliability == false) {
-					favorite_refineSearch++;
 					if(firstSearchRecord.getRequestInformation_requestHeaders_user_agent().matches("^Big Schedules.*iPhone' and '^Apache-HttpClient")){
-						favorite_refineSearch_mobile ++;
 					}else{
-						favorite_refineSearch_web++;
 					}
 				} else if (isWithoutFurtherAction == true && isRefineSearch == false && isSearchReliability == false
 						&& isShowmap == false) {
-					favorite_searchWithoutFurtherAction++;
 					if(firstSearchRecord.getRequestInformation_requestHeaders_user_agent().matches("^Big Schedules.*iPhone' and '^Apache-HttpClient")){
-						favorite_searchWithoutFurtherAction_mobile ++;
 					}else{
-						favorite_searchWithoutFurtherAction_web++;
 					}
 				} else {
-					favorite_others++;
 					if(firstSearchRecord.getRequestInformation_requestHeaders_user_agent().matches("^Big Schedules.*iPhone' and '^Apache-HttpClient")){
-						favorite_others_mobile ++;
 					}else{
-						favorite_others_web++;
 					}
 				}
 
