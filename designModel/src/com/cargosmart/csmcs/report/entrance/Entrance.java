284cb809983135e34ae46666ba50e46a56d07d40
guo <smile_struggle@outlook.com>
2015-11-18 09:01:04 +0800

    schedule job add update logic

--- designModel/src/com/cargosmart/csmcs/report/entrance/Entrance.java
+++ designModel/src/com/cargosmart/csmcs/report/entrance/Entrance.java
@@ -132,16 +132,8 @@ public class Entrance {
 	}
 
 	public static void main(String[] args) {
-		long begin = System.currentTimeMillis();
-		Entrance et = new Entrance();
-		et.executeLogic(false);
-		long end = System.currentTimeMillis();
-		logger.info("spent time " + (end - begin));
-		begin = System.currentTimeMillis();
-		et.executeLogic(true);
-		end = System.currentTimeMillis();
-		logger.info("spent time " + (end - begin));
-		logger.info("waiting for the next execute time");
+		logger.info("waiting for the right time to execute ");
+		new TimerManager();
 	}
 
 	public boolean inArray(String[] arr, String t) {
